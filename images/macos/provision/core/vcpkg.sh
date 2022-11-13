3f99d0451119f32dcf27c94dd3e266c45bbf9967
Bogdan Damian <bogdan.damian@akvelon.com>
2022-11-13 10:57:02 +0100

    macOS install vcpg from specific commit

--- images/macos/provision/core/vcpkg.sh
+++ images/macos/provision/core/vcpkg.sh
@@ -2,11 +2,17 @@
 source ~/utils/utils.sh
 
 # Set env variable for vcpkg
+GIT_COMMIT_HASH="299b1e17df207b4ea4b07c85af15cc7027f8cc25"
 VCPKG_INSTALLATION_ROOT=/usr/local/share/vcpkg
 echo "export VCPKG_INSTALLATION_ROOT=${VCPKG_INSTALLATION_ROOT}" | tee -a ~/.bashrc
 
 # Install vcpkg
 git clone https://github.com/Microsoft/vcpkg $VCPKG_INSTALLATION_ROOT
+
+pushd $VCPKG_INSTALLATION_ROOT
+git checkout $GIT_COMMIT_HASH
+popd
+
 $VCPKG_INSTALLATION_ROOT/bootstrap-vcpkg.sh
 $VCPKG_INSTALLATION_ROOT/vcpkg integrate install
 chmod -R 0777 $VCPKG_INSTALLATION_ROOT
